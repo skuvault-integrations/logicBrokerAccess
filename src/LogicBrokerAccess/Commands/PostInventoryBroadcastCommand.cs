d28f269019a18aa50ffc92f95dad5f0ed1d9d660
maxims-skuvault <maxim.strelsov@skuvault.com>
2020-03-17 09:11:20 -0400

    no message

---
+++ src/LogicBrokerAccess/Commands/PostInventoryBroadcastCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicBrokerAccess.Commands
+{
+	//TODO GUARD-4XX? Inv sync
+	//POST /api/v1/Inventory/BroadcastImport inventory from CSV.
+	//Request rate limited to 1 request every 60 seconds with bursts up to 2 requests.
+	public class PostInventoryBroadcastCommand
+	{
+
+	}
+}
